2760c31647fe1256567b5664b79e8067e2df9a2e
estebanfern <estebangfernandeza@gmail.com>
2022-12-30 11:09:21 -0300

    Prueba de retorno List/JSON

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		List<Student> list = new ArrayList<Student>();
+		list.add(new Student(1L, "Esteban", "estebangfernandeza@gmail.com",
+				LocalDate.of(2001, Month.DECEMBER, 24), 21));
+//		list.add("World");
+		return list;
 	}
 
 }
